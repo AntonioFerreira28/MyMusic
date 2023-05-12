971399e3e9f3938683dac6a3eb1b90ac8b2bebcf
AntonioFerreira28 <ramosfrr2003@outlook.com.br>
2023-05-12 10:13:32 -0300

    https://github.com/AntonioFerreira28/MyMusic

--- crude.js
+++ crude.js
@@ -12,7 +12,7 @@ window.addEventListener("load", () => {
 document.querySelector("#buscar").addEventListener("keyup", () =>{
     let busca = document.querySelector("#buscar").value
     let tarefasFiltradas = tarefas.filter((tarefa) => {
-        return  busca
+        return  tarefa.titulo.toLowerCase().includes(busca.toLowerCase())
 
     })
     filtrar(tarefasFiltradas)
